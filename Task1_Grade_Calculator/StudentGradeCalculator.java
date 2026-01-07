afdde58b4bda40c9b0a9a56cea4df6ed86f1e148
TechyDi <dkd1210202gmail.com>
2026-01-07 14:25:38 +0530

    working on core logic

--- Task1_Grade_Calculator/StudentGradeCalculator.java
+++ Task1_Grade_Calculator/StudentGradeCalculator.java
@@ -135,4 +135,11 @@ public class StudentGradeCalculator extends JFrame {
             else lblGradeDisplay.setForeground(Color.BLACK);
         }
     }
-
+    
+    private String computeGrade(double avg) {
+        if (avg >= 90) return "A";
+        else if (avg >= 80) return "B";
+        else if (avg >= 70) return "C";
+        else if (avg >= 60) return "D";
+        else return "F";
+    }
