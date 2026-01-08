cde443f22a362175c455feba2217c1473fa0b222
TechyDi <dkd1210202gmail.com>
2026-01-08 11:30:07 +0530

    Creating Logic

--- Task2_ATM_Interface/BankAccount.java
+++ Task2_ATM_Interface/BankAccount.java
@@ -1,4 +1,22 @@
 package CODSOFT.Task2_ATM_Interface;
 public class BankAccount {
+    private double balance;
+
+    public BankAccount(double initialBalance) {
+        this.balance = initialBalance;
+    }
+
+    public double getBalance() {
+        return balance;
+    }
+
+    public void deposit(double amount) {
+        if (amount > 0) {
+            balance += amount;
+        } else {
+            throw new IllegalArgumentException("Deposit amount must be positive.");
+        }
+    }
+
 
 }
