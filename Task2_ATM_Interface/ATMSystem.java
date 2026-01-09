f9b0d3590411fe08ca2cf2c06be757a59faa3a6c
TechyDi <dkd1210202gmail.com>
2026-01-09 20:50:11 +0530

    Adding Window setup

--- Task2_ATM_Interface/ATMSystem.java
+++ Task2_ATM_Interface/ATMSystem.java
@@ -13,9 +13,20 @@ public class ATMSystem extends JFrame {
     private JTextField txtAmount;
     private JLabel lblStatus;
 
-    //Color Palette (Modern Banking Theme) ---
+    //Color Palette (Banking Theme) ---
     private final Color COLOR_PRIMARY = new Color(44, 62, 80);    // Dark Navy
     private final Color COLOR_ACCENT = new Color(52, 152, 219);   // Blue
     private final Color COLOR_SUCCESS = new Color(46, 204, 113);  // Emerald Green
     private final Color COLOR_WARNING = new Color(230, 126, 34);  // Carrot Orange
     private final Color COLOR_BG = new Color(236, 240, 241);      // Soft Grey
+       
+    public ATMSystem() {
+        // Initialize Account with default 10000.00
+        userAccount = new BankAccount(10000.00);
+
+        // Window Setup
+        setTitle("CodSoft ATM Interface");
+        setSize(400, 550);
+        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        setLocationRelativeTo(null);
+        setLayout(new BorderLayout());
