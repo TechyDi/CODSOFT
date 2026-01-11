10480d66b086c7c067e7f24919331ae1d956f20c
TechyDi <dkd1210202gmail.com>
2026-01-11 18:55:15 +0530

    Initiating Task_3

---
+++ Task3_Student_Management_Sys/Student.java
@@ -0,0 +1,5 @@
+package CODSOFT.Task3_Student_Management_Sys;
+import java.io.Serializable;
+
+public class Student implements Serializable {
+    private static final long serialVersionUID = 1L; // Version control for serialization
